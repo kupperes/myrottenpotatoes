7686a50bf88feb1507ea295df95834f44c83d697
Sam Joseph <sam@neurogrid.com>
2015-08-07 00:45:04 +0000

    initial commit (partials and validations added)

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,50 @@
+class MoviesController < ApplicationController
+  def index
+    @movies = Movie.all
+  end
+  
+  def show
+    @movie = Movie.find params[:id]
+  end
+  
+  def new
+    @movie = Movie.new
+  end
+  
+  def create
+    @movie = Movie.create movie_params
+    if @movie.save
+      flash[:notice] = "#{@movie.title} was successfully created"
+      redirect_to movies_path
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @movie = Movie.find params[:id]
+  end
+  
+  def update
+    @movie = Movie.find params[:id]
+    if @movie.update_attributes movie_params
+      flash[:notice] = "#{@movie.title} was successfully updated"
+      redirect_to movie_path @movie
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @movie = Movie.find params[:id]
+    @movie.destroy
+    flash[:notice] = "Movie #{@movie.title} deleted"
+    redirect_to movies_path
+  end
+  
+  private
+  
+  def movie_params
+    params.require(:movie).permit(:title, :description, :rating, :release_date)
+  end
+end
